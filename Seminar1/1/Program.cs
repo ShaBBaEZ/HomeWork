f2151c6b543be92e87957914fc92fca31383da28
ShaBBa <Defend060496@gmail.com>
2023-03-25 02:41:52 +0300

    Готовое ДЗ Seminar1

---
+++ Seminar1/1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.Write("Введите первое число,");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число,");
+int y = int.Parse(Console.ReadLine()!);
+if(x > y)
+{
+    Console.WriteLine($"Число {x} большее,а числ {y} меньшее");
+}
+else
+{
+    Console.WriteLine($"Число {y} большее, а число {x} меньшее");
+}
---
+++ Seminar1/2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Clear();
+Console.Write("Введите первое число, ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число, ");
+int y = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число, ");
+int z = int.Parse(Console.ReadLine()!);
+int max = x;
+if ( x > max ) max = x;
+if ( y > max ) max = y;
+if ( z > max ) max = z;
+Console.Write("max = ");
+Console.WriteLine(max);
