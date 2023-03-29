5f8920c5b57604f70d3ec503190ff5f3adbae36b
ShaBBa <defend060496@gmail.com>
2023-03-29 15:37:05 +0300

    Seminar2

--- Seminar2/2/Program.cs
+++ Seminar2/2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+Console.Clear();
+int num = new Random().Next();
+Console.WriteLine($"{num}");
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+ while (num > 1000)
+ {
+    num = num / 10;
+ }
+ int x = num % 10;
+ Console.WriteLine($"Третья цифра числа {x}");
+}      
--- Seminar2/3/Program.cs
+++ Seminar2/3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.Clear();
+Console.Write("Введите день недели, ");
+int x = int.Parse(Console.ReadLine()!);
+if (x < 1 || x > 7)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else
+{
+    if (x > 0 && x < 6)
+    {
+        Console.WriteLine("Не выходной");
+    }
+    else
+    {
+        Console.WriteLine("Выходной");
+    }
+
+}
