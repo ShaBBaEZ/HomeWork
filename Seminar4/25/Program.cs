3bee051834f866c3a91960628b6e6d2c61572b43
ShaBBa <Defend060496@gmail.com>
2023-04-04 05:00:39 +0300

    Session4

---
+++ Seminar4/25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Число {a} в степени {b} = {Degree(a)}");
+
+
+int Degree(int a){
+    int result = 1;
+    for(int i = 0; i < b; i ++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
---
+++ Seminar4/27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.Clear();
+Console.WriteLine($"Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {x} = {GetSum(x)}");
+
+int GetSum(int x){
+    int sum = 0;
+    while(x > 0){
+        int y = x % 10;
+        sum = sum + y;
+        x = x / 10;
+    }
+    return sum;
+}
