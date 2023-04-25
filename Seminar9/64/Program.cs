d34cd1443409d8eba65e7cfe773e449b508d1e12
ShaBBa <defend060496@gmail.com>
2023-04-25 16:30:43 +0300

    Seminar9

---
+++ Seminar9/64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите значение N: ");
+int N = int.Parse(ReadLine()!);
+WriteNow(N,1);
+
+void WriteNow(int N, int i)
+{
+    if(i <= N)
+    {
+        Write(N + " ");
+        N--;
+        WriteNow(N, i);
+    }
+}
---
+++ Seminar9/66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите значение M: ");
+int M = int.Parse(ReadLine()!);
+Write("Введите значение N: ");
+int N = int.Parse(ReadLine()!);
+Write(SUM(M,N));
+
+int SUM(int i, int j)
+{
+    int sum = 0;
+    while (i <= j)
+    {
+        sum+= i;
+        i++;
+        SUM(i,j);
+    }
+    return sum;
+}
+
